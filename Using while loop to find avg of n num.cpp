52d4fcdfa0ecfcfe70c29ae366d451211994599a
C. Aditya Venkat Reddy <93367856+aditya2353@users.noreply.github.com>
2021-12-24 13:15:51 +0530

    Add files via upload

---
+++ Using while loop to find avg of n num.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+main()
+{
+	int i=1,sum=0,n,k,avg;
+	printf("enter how many numbers average");
+	scanf("%d",&n);
+	{
+		scanf("%d",&k);
+		sum=sum=k;
+		i++;
+	}
+	printf("sum is %d",sum);
+	avg=(float)(sum/n);
+	printf("Average is %d",avg);
+}
